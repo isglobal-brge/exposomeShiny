4827e96f0c7ed483feada11c54f12545de2eaae1
ESCRI11 <escri11@yahoo.es>
2020-09-29 15:32:36 +0200

    installer updated

--- installer.R
+++ installer.R
@@ -1,16 +1,20 @@
-pkg_list <- c("shiny", "shinyBS", "mice", "DT", "ggplot2", "data.table", "truncdist", "shinyalert", "shinydashboard")
+pkg_list <- c("shiny", "shinyBS", "mice", "DT", "ggplot2", "data.table", "truncdist", "shinyalert", "shinydashboard", "devtools")
 
 for(pkg in pkg_list){
   if (!require(pkg, character.only = TRUE)) {
     install.packages(pkg, dependencies = TRUE )
   }
 }
-  
+
+devtools::install_version('BiocManager', version = '1.30.10', repos = 'http://cran.us.r-project.org')
+
 pkg_list_bioc <- c("rexposome", "omicRexposome", "MultiDataSet", "TxDb.Hsapiens.UCSC.hg19.knownGene", 
-                   "org.Hs.eg.db", "GenomicRanges", "CTDquerier")
+                   "org.Hs.eg.db", "GenomicRanges")
 
 for(pkg in pkg_list_bioc){
   if (!require(pkg, character.only = TRUE)) {
     BiocManager::install(pkg)
   }
 }
+
+devtools::install_github("isglobal-brge/CTDquerier")
